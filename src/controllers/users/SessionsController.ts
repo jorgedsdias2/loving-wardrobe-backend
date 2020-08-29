7851896591efb5b5949c08b0031a110b2bc53c93
Jorge <jorgedsdias@gmail.com>
2020-08-29 20:49:16 -0300

    Ajustes

--- src/controllers/users/SessionsController.ts
+++ src/controllers/users/SessionsController.ts
@@ -1,17 +1,7 @@
 import { Request, Response } from 'express';
 
-import UsersRepository from '../../database/repositories/users/UsersRepository';
-
 export default class SessionsController {
   public async index(req: Request, res: Response): Promise<Response> {
-    const usersRepository = new UsersRepository();
-
-    usersRepository.create({
-      username: 'Jorge Dias',
-      email: 'jorge.dias@gmail.com',
-      password: '123456',
-    });
-
     return res.json({ message: 'chamou funcao' });
   }
 }
