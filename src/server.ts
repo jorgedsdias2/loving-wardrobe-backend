3758554a40000dcfeac188c97824ea793daedee1
Jorge <jorgedsdias@gmail.com>
2020-08-29 12:15:10 -0300

    Arrumando estrutura inicial

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,33 @@
-import express from 'express';
+import 'dotenv/config';
+
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
+import AppError from './errors/AppError';
 import routes from './routes';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, _: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  console.log(error);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 const port = process.env.PORT || 3333;
 app.listen(port, () => {
-  console.log(`🚀 Server started on port ${port}!`);
+  console.log(`🚀 Server started on port ${port}!!!`);
 });
